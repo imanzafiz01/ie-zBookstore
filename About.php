144871c112a1d2bd2421a1f71b04aa306401c18a
Iman Zafiz <imanzafiz01@gmail.com>
2022-03-24 13:21:24 +0800

    commit on master

---
+++ About.php
@@ -0,0 +1,294 @@
+<?php
+session_start();
+include "dbconnect.php";
+
+if (isset($_GET['Message'])) {
+    print '<script type="text/javascript">
+               alert("' . $_GET['Message'] . '");
+           </script>';
+}
+
+if (isset($_GET['response'])) {
+    print '<script type="text/javascript">
+               alert("' . $_GET['response'] . '");
+           </script>';
+}
+
+if(isset($_POST['submit']))
+{
+  if($_POST['submit']=="login")
+  { 
+        $username=$_POST['login_username'];
+        $password=$_POST['login_password'];
+        $query = "SELECT * from users where UserName ='$username' AND Password='$password'";
+        $result = mysqli_query($con,$query)or die(mysql_error());
+        if(mysqli_num_rows($result) > 0)
+        {
+             $row = mysqli_fetch_assoc($result);
+             $_SESSION['user']=$row['UserName'];
+             print'
+                <script type="text/javascript">alert("successfully logged in!!!");</script>
+                  ';
+        }
+        else
+        {    print'
+              <script type="text/javascript">alert("Incorrect Username Or Password!!");</script>
+                  ';
+        }
+  }
+  else if($_POST['submit']=="register")
+  {
+        $username=$_POST['register_username'];
+        $password=$_POST['register_password'];
+        $query="select * from users where UserName = '$username'";
+        $result=mysqli_query($con,$query) or die(mysql_error);
+        if(mysqli_num_rows($result)>0)
+        {   
+               print'
+               <script type="text/javascript">alert("username is taken");</script>
+                    ';
+
+        }
+        else
+        {
+          $query ="INSERT INTO users VALUES ('$username','$password')";
+          $result=mysqli_query($con,$query);
+          print'
+                <script type="text/javascript">
+                 alert("Successfully Registered!!!");
+                </script>
+               ';
+        }
+  }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Books">
+    <meta name="author" content="Shivangi Gupta">
+    <title>Online Bookstore</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/my.css" rel="stylesheet">
+    <style>
+      .modal-header {background:#D67B22;color:#fff;font-weight:800;}
+      .modal-body{font-weight:800;}
+      .modal-body ul{list-style:none;}
+      .modal .btn {background:#D67B22;color:#fff;}
+      .modal a{color:#D67B22;}
+      .modal-backdrop {position:inherit !important;}
+       #login_button,#register_button{background:none;color:#D67B22!important;}       
+       #query_button {position:fixed;right:0px;bottom:0px;padding:10px 80px;
+                      background-color:#D67B22;color:#fff;border-color:#f05f40;border-radius:2px;}
+  	@media(max-width:767px){
+        #query_button {padding: 5px 20px;}
+  	}
+      #books {margin-bottom:50px;}
+
+     @media only screen and (width: 767px) { body{margin-top:150px;}}
+        #books .row{margin-top:20px;margin-bottom:10px;font-weight:800;}
+        @media only screen and (max-width: 760px) { #books .row{margin-top:10px;}}
+    </style>
+</head>
+<body>
+  <nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php" style="padding: 1px;"><p  style="width: 147px;margin: 10px;">IE-Z BOOKSTORE<p></a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+         <ul class="nav navbar-nav navbar-right">
+        <?php
+        if(!isset($_SESSION['user']))
+          {
+            echo'
+            <li>
+                <button type="button" id="login_button" class="btn btn-lg" data-toggle="modal" data-target="#login">Login</button>
+                  <div id="login" class="modal fade" role="dialog">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                <h4 class="modal-title text-center">Login Form</h4>
+                            </div>
+                            <div class="modal-body">
+                                          <form class="form" role="form" method="post" action="index.php" accept-charset="UTF-8">
+                                              <div class="form-group">
+                                                  <label class="sr-only" for="username">Username</label>
+                                                  <input type="text" name="login_username" class="form-control" placeholder="Username or Email" required>
+                                              </div>
+                                              <div class="form-group">
+                                                  <label class="sr-only" for="password">Password</label>
+                                                  <input type="password" name="login_password" class="form-control"  placeholder="Password" required>
+                                              </div>
+                                              <div class="form-group">
+                                                  <button type="submit" name="submit" value="login" class="btn btn-block">
+                                                      Sign in
+                                                  </button>
+                                              </div>
+                                          </form>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+            </li>
+            <li>
+              <button type="button" id="register_button" class="btn btn-lg" data-toggle="modal" data-target="#register">Sign Up</button>
+                <div id="register" class="modal fade" role="dialog">
+                  <div class="modal-dialog">
+                      <div class="modal-content">
+                          <div class="modal-header">
+                              <button type="button" class="close" data-dismiss="modal">&times;</button>
+                              <h4 class="modal-title text-center">Member Registration Form</h4>
+                          </div>
+                          <div class="modal-body">
+                                        <form class="form" role="form" method="post" action="index.php" accept-charset="UTF-8">
+                                            <div class="form-group">
+                                                <label class="sr-only" for="username">Username</label>
+                                                <input type="text" name="register_username" class="form-control" placeholder="Username or Email" required>
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="sr-only" for="password">Password</label>
+                                                <input type="password" name="register_password" class="form-control"  placeholder="Password" required>
+                                            </div>
+                                            <div class="form-group">
+                                                <button type="submit" name="submit" value="register" class="btn btn-block">
+                                                    Sign Up
+                                                </button>
+                                            </div>
+                                        </form>
+                          </div>
+                          <div class="modal-footer">
+                              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                          </div>
+                      </div>
+                  </div>
+                </div>
+            </li>';
+          } 
+        else
+          {   echo' <li> <a href="#" class="btn btn-lg"> Hello ' .$_SESSION['user']. '</a></li>
+                    <li> <a href="cart.php" class="btn btn-lg"> Cart </a> </li>;
+                    <li> <a href="About.php" class="btn btn-lg"> About </a> </li>;
+                    <li> <a href="destroy.php" class="btn btn-lg"> LogOut </a> </li>';
+               
+          }
+?>
+
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+    
+    <section>
+        <p size='large' align='center'><br>This is Web Application Programming Project<br>
+           for subject CSC204 Programme CT203 KUPTM<br>
+           By MUHAMMAD NUR IMAN BIN ZAFIZ<br>
+           AM2107009334<br></p>
+    </section><br><br><br><br><br><br><br><br><br><br><br><br>
+
+  <footer style="margin-left:-6%;margin-right:-6%;">
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col-sm-1 col-md-1 col-lg-1">
+              </div>
+              <div class="col-sm-7 col-md-5 col-lg-5">
+                  <div class="row text-center">
+                      <h2>Copyright</h2>
+                      <hr class="primary">
+                      <p>2022</p>
+                  </div>
+                  
+              </div>
+              <div class="hidden-sm-down col-md-2 col-lg-2">
+              </div>
+              <div class="col-sm-4 col-md-3 col-lg-3 text-center">
+                  <h2 style="color:#D67B22;">Follow Us At</h2>
+                  <div>
+                      <a href="https://twitter.com">
+                      <img title="Twitter" alt="Twitter" src="img/social/twitter.png" width="35" height="35" />
+                      </a>
+                      <a href="https://www.linkedin.com">
+                      <img title="LinkedIn" alt="LinkedIn" src="img/social/linkedin.png" width="35" height="35" />
+                      </a>
+                      <a href="https://www.facebook.com">
+                      <img title="Facebook" alt="Facebook" src="img/social/facebook.png" width="35" height="35" />
+                      </a>
+                      <a href="https://plus.google.com">
+                      <img title="google+" alt="google+" src="img/social/google.jpg" width="35" height="35" />
+                      </a>
+                      <a href="https://www.pinterest.com">
+                      <img title="Pinterest" alt="Pinterest" src="img/social/pinterest.jpg" width="35" height="35" />
+                      </a>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </footer>
+
+<div class="container">
+  <!-- Trigger the modal with a button -->
+  <button type="button" id="query_button" class="btn btn-lg" data-toggle="modal" data-target="#query">FAQ</button>
+  <!-- Modal -->
+  <div class="modal fade" id="query" role="dialog">
+    <div class="modal-dialog">
+      <div class="modal-content">
+          <div class="modal-header text-center">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Ask your question here</h4>
+          </div>
+          <div class="modal-body">           
+                    <form method="post" action="query.php" class="form" role="form">
+                        <div class="form-group">
+                             <label class="sr-only" for="name">Name</label>
+                             <input type="text" class="form-control"  placeholder="Your Name" name="sender" required>
+                        </div>
+                        <div class="form-group">
+                             <label class="sr-only" for="email">Email</label>
+                             <input type="email" class="form-control" placeholder="abc@gmail.com" name="senderEmail" required>
+                        </div>
+                        <div class="form-group">
+                             <label class="sr-only" for="query">Message</label>
+                             <textarea class="form-control" rows="5" cols="30" name="message" placeholder="Your Question or Feedback" required></textarea>
+                        </div>
+                        <div class="form-group">
+                              <button type="submit" name="submit" value="query" class="btn btn-block">
+                                                              Send Question
+                               </button>
+                        </div>
+                    </form>
+          </div>
+          <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+      </div>
+    </div>  
+  </div>
+</div>
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="js/bootstrap.min.js"></script>
+</body>
+</html>	
---
+++ query.php
@@ -0,0 +1,19 @@
+<?php
+if($_POST["submit"]=="query") 
+{
+    $recipient="imanzafiz01@gmail.com";
+    $subject="Answer from bookstore";
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $resSub = "Confirmation of receiving your feedback";
+    $resBody= "Dear ". $sender ."\n\nThanks for reaching us.\nThis is to inform you that we have received your feedback. We will get back to you asap.";
+    $note="\n\nNote : This is an auto-generated mail do not reply to this.";
+    $resBody=$resBody . $note;
+    mail($senderEmail , $resSub , $resBody);   
+    header("location: index.php?response="."Your Message has been successfully sent! Our customer executive would respond shortly."); 
+}
+?>	
